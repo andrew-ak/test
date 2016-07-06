719d51d129da4bb40d6d85c3f41f63a8c4f56dc7
andrew-ak <andrew.a.kuzmenko@gmail.com>
2016-07-06 18:06:21 +0300

    all

---
+++ apps/backend/controllers/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Backend\Controllers;
+
+class NewsController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+    public function showAction(){
+    	
+    }
+}
